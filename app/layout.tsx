9c9c277abc456853ae1a238333f34389ab679983
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2024-02-02 19:25:41 -0300

    feat: toaster in layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Footer } from "./_components/Footer";
 import AuthProvider from "./_providers/auth";
+import { Toaster } from "./_components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} dark`}><AuthProvider>
-        {children}<Footer /></AuthProvider></body>
+        {children}<Toaster /><Footer /></AuthProvider></body>
     </html>
   );
 }
