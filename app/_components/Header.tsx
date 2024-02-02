a572ddf066f4e5b8ffe179dbc1923eaaa0230156
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2024-02-02 00:33:28 -0300

    feat: side menu for user login

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,17 +1,33 @@
-import Image from "next/image"
-import { Card, CardContent } from "./ui/card"
-import { Button } from "./ui/button"
-import { MenuIcon } from "lucide-react"
+"use client";
+
+import Image from "next/image";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { SideMenu } from "./SideMenu";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <Card>
-      <CardContent className="p-5 flex flex-row items-center justify-between">
-        <Image src='/logo.png' alt="Barber shop" height={18} width={120} />
-        <Button variant='outline' size='icon'>
-          <MenuIcon size={16} />
-        </Button>
+      <CardContent className="p-5 justify-between items-center flex flex-row">
+        <Link href="/">
+          <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+        </Link>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <MenuIcon size={16} />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent className="p-0">
+            <SideMenu />
+          </SheetContent>
+        </Sheet>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
+
