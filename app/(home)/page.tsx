c8e5d055a3ec27864eea08aa684359a41e381b88
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2024-02-02 19:26:41 -0300

    feat: action and home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,52 +1,85 @@
-import { Header } from "../_components/Header";
-import { format } from 'date-fns'
+import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
-import { Search } from "./_components/Search";
-import { BookingItem } from "../_components/BookingItem";
 import { db } from "../_lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { Header } from "../_components/Header";
+import { BookingItem } from "../_components/BookingItem";
+import { Search } from "./_components/Search";
 import { BarbershopItem } from "./_components/BarbershopItem";
-import { Barbershop } from "@prisma/client"
 
 export default async function Home() {
-  const barbershops: Barbershop[] = await db.barbershop.findMany()
+  const session = await getServerSession(authOptions);
+
+  const [barbershops, confirmedBookings] = await Promise.all([
+    db.barbershop.findMany({}),
+    session?.user
+      ? db.booking.findMany({
+        where: {
+          userId: (session.user as any).id,
+          date: {
+            gte: new Date(),
+          },
+        },
+        include: {
+          service: true,
+          barbershop: true,
+        },
+      })
+      : Promise.resolve([]),
+  ]);
 
   return (
-    <main className="">
+    <div>
       <Header />
 
-      <section className="p-5">
-        <h2 className="text-xl font-bold">Olá, Thiago!</h2>
-        <p className="capitalize text-sm">{format(Date.now(), "EEEE',' d 'de' MMMM", {
-          locale: ptBR,
-        })}</p>
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">
+          {session?.user ? `Olá, ${session.user.name?.split(" ")[0]}!` : "Olá! Vamos agendar um corte hoje?"}
+        </h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
 
-      </section>
-
-      <section className="px-5 mt-6">
+      <div className="px-5 mt-6">
         <Search />
-      </section>
+      </div>
 
-      <section className="px-5 mt-6">
-        <h2 className="mb-3 text-xs uppercase text-gray-400">Agendamentos</h2>
-        <BookingItem />
-      </section>
+      <div className="mt-6">
+        {confirmedBookings.length > 0 && (
+          <>
+            <h2 className="pl-5 text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+            <div className="px-5 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+              {confirmedBookings.map((booking) => (
+                <BookingItem key={booking.id} booking={booking} />
+              ))}
+            </div>
+          </>
+        )}
+      </div>
 
-      <article className="px-5 mt-6">
-        <h2 className="mb-3 text-xs uppercase text-gray-400">Recomendados</h2>
+      <div className="mt-6">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
 
-        <section className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-          {barbershops.map((barbershop) => <BarbershopItem key={barbershop.id} barbershop={barbershop} />)}
-        </section>
-      </article>
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
 
-      <article className="px-5 mt-6 mb-[4.5rem]">
-        <h2 className="mb-3 text-xs uppercase text-gray-400">Populares</h2>
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
 
-        <section className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-          {barbershops.map((barbershop) => <BarbershopItem key={barbershop.id} barbershop={barbershop} />)}
-        </section>
-      </article>
-
-    </main>
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
