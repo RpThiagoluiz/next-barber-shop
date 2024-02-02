903e6af6a4599c267424f1c28b38f6d15d5ad457
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2024-02-01 23:35:42 -0300

    feat: action to barbershops root

--- app/(home)/_components/BarbershopItem.tsx
+++ app/(home)/_components/BarbershopItem.tsx
@@ -6,12 +6,19 @@ import { Card, CardContent } from "@/app/_components/ui/card"
 import { Barbershop } from "@prisma/client"
 import { StarIcon } from "lucide-react"
 import Image from "next/image"
+import { useRouter } from "next/navigation";
 
 type BarbershopItemProps = {
   barbershop: Barbershop
 }
 
 export const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
+
+  const router = useRouter()
+
+  const handleBooking = () => {
+    router.push(`/barbershops/${barbershop.id}`)
+  }
   return (
     <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
       <CardContent className="px-1 py-0">
@@ -36,7 +43,7 @@ export const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
         <div className="px-2 pb-3">
           <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">{barbershop.name}</h2>
           <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">{barbershop.address}</p>
-          <Button className="w-full mt-3" variant="secondary" onClick={() => { }}>
+          <Button className="w-full mt-3" variant="secondary" onClick={handleBooking}>
             Reservar
           </Button>
         </div>
